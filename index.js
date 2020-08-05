60e10401542cc310db9c70f056600753bb6bd71a
bsitenga <sitenga@usc.edu>
2020-08-04 22:57:20 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// all API endpoints
+app.get('/', (req, res) => {
+  console.log("first route");
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Playlist generator listening on ${port}`);
