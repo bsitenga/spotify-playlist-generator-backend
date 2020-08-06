cf32786c3531a1021f237670243894fa904cbb05
bsitenga <sitenga@usc.edu>
2020-08-06 15:32:55 -0400

    created right tab skeleton

--- client/src/components/homepage/Hero.js
+++ client/src/components/homepage/Hero.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../App.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function Hero() {
+	const [ tracks, setTracks ] = useState([]);
+
 	return (
 		<div className="hero-master-container">
 			<Container fluid>
 				<Row className="hero-row">
 					<Col md={5} className="hero-left">
 						<div>
-							<h1 className="hero-header">Log in to Spotify to save your playlists.</h1>
-              <p className="hero-paragraph">stuff</p>
-              <button className="hero-button">Login with Spotify</button>
-            </div>
+							<h1 className="hero-header">Login to Spotify to save your playlists.</h1>
+							<p className="hero-paragraph">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit risus non nulla
+								aliquet vestibulum. Phasellus egestas dolor massa, ut mollis leo tempor interdum.
+								Curabitur finibus.
+							</p>
+							<button className="hero-button">Login with Spotify</button>
+						</div>
 					</Col>
-					<Col md={7}>col 2</Col>
+					<Col md={7}>
+						<div className="hero-right">
+              <h3>Generate a playlist</h3>
+							<input placeholder="Enter a track title" />
+							<div>
+								<p>Current tracks</p>
+								<div className="current-tracks">
+									{tracks.map((item) => {
+										return (
+											<p>
+												{item.name} - {item.artist}
+											</p>
+										);
+									})}
+								</div>
+							</div>
+              <button>Generate</button>
+						</div>
+					</Col>
 				</Row>
 			</Container>
 		</div>
