acebe24ae2fdfb37848d8e5eb4176c27e3f7b165
bsitenga <sitenga@usc.edu>
2020-08-07 12:45:51 -0400

    created files for new tabs

---
+++ client/src/components/about/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+function About() {
+	return (
+		<div className="about-master-container">
+			About
+		</div>
+	);
+}
+
+export default About;
---
+++ client/src/components/developers/Developers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+function Developers() {
+	return (
+		<div className="developers-master-container">
+			Developers
+		</div>
+	);
+}
+
+export default Developers;
