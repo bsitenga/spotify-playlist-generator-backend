0326e408e004019b30289987b25aa9fd1f84088c
bsitenga <sitenga@usc.edu>
2020-08-08 14:24:54 -0400

    removed passport from index.js

--- index.js
+++ index.js
@@ -1,44 +1,14 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors')
-const passport = require('passport');
-const SpotifyStrategy = require('passport-spotify').Strategy;
-
 const app = express();
 
 app.use(cors({origin: true, credentials: true}));
-
-passport.use(
-  new SpotifyStrategy(
-    {
-      clientID: process.env.clientID,
-      clientSecret: process.env.clientSecret,
-      callbackURL: 'http://localhost:5000/auth/spotify/callback'
-    },
-    function(accessToken, refreshToken, expires_in, profile, done) {
-      console.log('profile:', profile)
-    }
-  )
-);
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // all API endpoints below
-
-app.get('/auth/spotify', passport.authenticate('spotify'), function(req, res) {
-  
-});
-
-app.get(
-    '/auth/spotify/callback',
-    passport.authenticate('spotify', { failureRedirect: '/' }),
-    function(req, res) {
-      console.log('successful authentication');
-      // Successful authentication, redirect home.
-      res.redirect('/about');
-    }
-  );
 
 //Searches for song
 app.get('/search', (req, res) => {
