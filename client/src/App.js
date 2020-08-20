bf9ee93231520b226f5dc47537cd14092c7d312c
bsitenga <bsitenga@gmail.com>
2020-08-20 16:16:04 -0400

    receives auth token in useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePageMain from './components/homepage/HomePageMain.js';
 import Navigation from './components/navbar/Navigation.js';
--- client/src/components/homepage/HeroAuthenticated.js
+++ client/src/components/homepage/HeroAuthenticated.js
@@ -1,11 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../App.css';
+import axios from 'axios';
+import qs from 'qs';
 
 function HeroAuthenticated() {
+  const [accessToken, setAccessToken] = useState('');
+
   useEffect(() => {
     let code = getParams(window.location.href).code;
     if (code) {
-
+      axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: {
+          grant_type: "authorization_code",
+          code: code,
+          redirect_uri: 'http://localhost:3000/authenticated',
+          client_id: 'f0c3aa26b442470db2737973a26efc0a',
+          client_secret: '08760bf9ddcb40a199d1419291a73942'
+        },
+        data: qs.stringify({
+          grant_type: "authorization_code",
+          code: code,
+          redirect_uri: 'http://localhost:3000/authenticated',
+          client_id: 'f0c3aa26b442470db2737973a26efc0a',
+          client_secret: '08760bf9ddcb40a199d1419291a73942'
+        }),
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      })
+        .then(function (response) {
+          setAccessToken(response);
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log("post error:", error.response)
+        })
     } else {
       // TODO: replace url with final url after deploying
       window.location.replace("http://localhost:3000")
@@ -25,10 +56,22 @@ function HeroAuthenticated() {
     return params;
   }
 
+  const AuthenticatedTokenPage = () => {
+    return <div className="authenticated-token-page">
+      <h3>Choose Up to Five Songs</h3>
+    </div>
+  }
+
+  const LoadingError = () => {
+    return <div className="loading-page">
+      Authentication error
+      Please sign in again
+    </div>
+  }
+
   return (
     <div className="hero-authenticated">
-      <h3>Choose Up to Five Songs</h3>
-      <button onClick={() => console.log(this.props.match.params.id)}>test</button>
+      {accessToken ? AuthenticatedTokenPage() : LoadingError()}
     </div>
   );
 }
