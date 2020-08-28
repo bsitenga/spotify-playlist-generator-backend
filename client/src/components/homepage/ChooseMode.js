568082055f8274f59d1318e34f492eda471d8463
bsitenga <bsitenga@gmail.com>
2020-08-27 23:53:29 -0400

    added first slider and state for song values

--- client/src/components/homepage/ChooseMode.js
+++ client/src/components/homepage/ChooseMode.js
@@ -10,6 +10,8 @@ function ChooseMode(props) {
     const [searchResults, setSearchResults] = useState(false);
     const [playButtons, setPlayButtons] = useState([]);
     const [errorMessage, setErrorMessage] = useState("");
+    const [songVals, setSongVals] = useState([0, 0, 0, 0, 0, 0]);
+    const [acousticness, setAcousticness] = useState(.5);
 
     const accessToken = props.accessToken;
 
@@ -117,8 +119,10 @@ function ChooseMode(props) {
                     <Spinner style={{ color: '#1db954', margin: '2vh auto 2vh' }} animation="border" role="status"></Spinner>}
             </div> : ''}
             <div className="added-results">
-                <p>{errorMessage}</p>
+                <p style={{ color: "#777" }}>{errorMessage}</p>
                 <h5>Added Tracks</h5>
+                <hr className="choose-divider"></hr>
+                {tracks[0] ? "" : <p>Add a track!</p>}
                 {tracks.map((item, index) => {
                     return <div key={index} className='added-track'>
                         <img src={item.image} />
@@ -132,7 +136,21 @@ function ChooseMode(props) {
                 })}
             </div>
             <div className="set-filters">
-                Set Filters
+                <h3>Set Filters</h3>
+                <hr className="choose-divider"></hr>
+                <div className="slider-master">
+                    <div className="slider-container">
+                        <label for="acousticness-slider">Acousticness</label>
+                        <input className="acousticness-slider slider" type="range" min="0" max="100" value={acousticness * 100} onChange={(e) => setAcousticness(e.target.value / 100)}></input>
+                        <div className="bound-values">
+                            <span className="left-val">0</span>
+                            <span className="right-val">1</span>
+                        </div>
+                    </div>
+                    <div className="song-val">
+                        Average Value: {songVals[0]}
+                    </div>
+                </div>
             </div>
             <button>Generate</button>
         </div>
